7ec61eb28e7c92d177a882bc624ef5d1cb6157a7
Seun Dawodu <seun.dawodu@rhodiumsoftware.co.uk>
2017-10-21 15:20:01 +0100

    Differentiate trainer/trainee roles
    Modify some model relationships

--- tracker/admin.py
+++ tracker/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from tracker.models import *
 # Register your models here.
 admin.site.register(Exercise)
+admin.site.register(Person)
 admin.site.register(Plan)
 admin.site.register(Phase)
 admin.site.register(Period)
